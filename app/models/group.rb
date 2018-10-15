77233f8835ab079bd64cad8a79809d8df0f98890
Ivan Rahn <ivan.rahn@gmail.com>
2018-10-15 13:13:41 +1100

    many to many

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,10 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
+
+  class << self
+    def ruby_users
+      find_by(name: "ruby").users
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :tweets
-  has_many :group_users
-  has_many :groups, through: :group_users
+  has_many :tweets, dependent: :destroy
+  has_many :group_users, dependent: :destroy
+  has_many :groups, through: :group_users, dependent: :destroy
 
   def self.by_name_and_number(name:, number:)
     self.where(name: "#{name} #{number}")
